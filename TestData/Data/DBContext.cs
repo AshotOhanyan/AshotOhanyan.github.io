2b816961a487af8d7f7e1f38955c7ae352f8db57
Ashot <Ashot@DESKTOP-TI6QQ6L>
2023-06-15 22:06:34 +0400

    Add project files.

---
+++ TestData/Data/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestData.DbModels;
+
+namespace TestData.Data
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer("YourConnectionString");
+        }
+    }
+}
---
+++ TestData/DbModels/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData.DbModels
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        
+        public virtual List<Game> Games { get; set; }
+    }
+}
